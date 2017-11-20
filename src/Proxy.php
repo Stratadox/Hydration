fab49b865d0827a7aacae6cbb6e66c5954090a78
Stratadox <jesse@stratadox.com>
2017-11-20 23:51:17 +0100

    Removes public proxy constants

--- src/Proxy.php
+++ src/Proxy.php
@@ -4,12 +4,6 @@ namespace Stratadox\Hydration;
 
 interface Proxy
 {
-    public const LOADER    = 'proxyInstanceLoader';
-    public const SETTER    = 'proxyOwnerUpdater';
-    public const OWNER     = 'proxyOwner';
-    public const PROPERTY  = 'proxyIsReferencedBy';
-    public const OFFSET    = 'proxyIsLocatedAtOffset';
-
     /**
      * Lazily load and return the proxied object.
      *
