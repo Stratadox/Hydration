9e9d8b3d81c9f7b741e3e427862439ebb412aadd
Stratadox <jesse@stratadox.com>
2017-11-20 23:54:26 +0100

    Add interface for the updater factories

---
+++ src/ProducesOwnerUpdaters.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Stratadox\Hydration;
+
+interface ProducesOwnerUpdaters
+{
+    /**
+     * Produce an instance that @see UpdatesTheProxyOwner when the proxy is loaded.
+     *
+     * @param object     $theOwner      The object this proxy gets bound to.
+     * @param string     $ofTheProperty The property this proxy gets bound to.
+     * @param mixed|null $atPosition    The position of the proxy in the collection.
+     * @return UpdatesTheProxyOwner     The instance responsible for updating the property.
+     */
+    public function makeUpdaterFor(
+        $theOwner,
+        string $ofTheProperty,
+        $atPosition = null
+    ) : UpdatesTheProxyOwner;
+}
