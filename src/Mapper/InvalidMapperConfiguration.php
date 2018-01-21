a19b13e3493224bf587a5895f1988dc26ede8b5b
Stratadox <jesse@stratadox.com>
2018-01-21 14:38:30 +0100

    Incorporate the Mapper interfaces

---
+++ src/Mapper/InvalidMapperConfiguration.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stratadox\Hydration\Mapper;
+
+use Throwable;
+
+/**
+ * Indicates that the mapper is configured incorrectly.
+ *
+ * @author Stratadox
+ * @package Stratadox\Hydrate
+ */
+interface InvalidMapperConfiguration extends Throwable
+{
+}
